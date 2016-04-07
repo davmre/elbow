271129d883fddeaef352c957fd39d2c38b27b730
Dave Moore <dmoore@cs.berkeley.edu>
2016-04-06 17:05:38 -0700

    initial checkin of marginalized indian buffet process

--- bayesflow/dists.py
+++ bayesflow/dists.py
@@ -101,9 +101,11 @@ def gaussian_kl(mu_p, sigma2_p, mu_q=None, sigma2_q=None):
 
     if mu_q is None and sigma2_q is None:
         # assume standard normal
-        kl = .5 * (tf.square(mu_p) + sigma2_p - 1 - tf.log(sigma2_p))
+        kl = .5 * (tf.square(mu_p) + sigma2_p - 1.0 - tf.log(sigma2_p))
     else:
-        kl = .5 * (tf.square(mu_p - mu_q)/sigma2_q + sigma2_p/sigma2_q - 1 - tf.log(sigma2_p) + tf.log(sigma2_q))
+        logsp = tf.log(sigma2_p, name="logsp")
+        logsq =  tf.log(sigma2_q, name="logsq")
+        kl = .5 * (tf.square(mu_p - mu_q)/sigma2_q + sigma2_p/sigma2_q - 1.0 - logsp + logsq)
         
     return kl
 
@@ -196,6 +198,22 @@ def beta_log_density(x, alpha=1.0, beta=1.0):
     log_density = (alpha - 1) * tf.log(x) + (beta-1) * tf.log(1-x) - log_z
     return log_density
 
+def bernoulli_kl(p, q, clip_finite=True, ):
+
+    if clip_finite:
+        lp = tf.log(tf.clip_by_value(p, 1e-45, 1.0), name="bernoulli_logp")
+        lp1 = tf.log(tf.clip_by_value(1.0-p, 1e-45, 1.0), name="bernoulli_log1p")
+        lq = tf.log(tf.clip_by_value(q, 1e-45, 1.0), name="bernoulli_logq")
+        lq1 = tf.log(tf.clip_by_value(1.0-q, 1e-45, 1.0), name="bernoulli_log1q")
+    else:
+        lp = tf.log(p, name="bernoulli_logp")
+        lp1 = tf.log(1.0-p, name="bernoulli_log1p")
+        lq = tf.log(p, name="bernoulli_logq")
+        lq1 = tf.log(1.0-p, name="bernoulli_log1q")
+        
+    kl = p * (lp - lq) + (1.0-p) * (lp1 - lq1)
+    return kl
+    
 def bernoulli_log_density(x, p, clip_finite=True):
 
     if clip_finite:
