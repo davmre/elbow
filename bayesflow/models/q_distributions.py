904a4eb406ccacc9f23b05e4dc4d9468e801a634
Dave Moore <dmoore@cs.berkeley.edu>
2016-04-17 18:13:31 -0700

    add delta and pointwise-transformed distributions

--- bayesflow/models/q_distributions.py
+++ bayesflow/models/q_distributions.py
@@ -37,10 +37,54 @@ class ObservedQDistribution(QDistribution):
     
     def entropy(self):
         return 0.0
+
+class DeltaQDistribution(QDistribution):
+    def __init__(self, tf_value):
+
+        shape = util.extract_shape(tf_value)
+        super(DeltaQDistribution, self).__init__(shape=shape)
+        
+        self.mean = tf_value
+        self.stddev = tf.zeros_like(self.mean, name="stddev")
+        self.variance = tf.identity(self.stddev)
+        self.sample = self.mean 
+        
+    def sample_stochastic_inputs(self):
+        return {}
     
+    def entropy(self):
+        return 0.0
+
+class PointwiseTransformedQDistribution(QDistribution):
+    def __init__(self, parent_q, transform, implicit=False):
+
+        
+        super(PointwiseTransformedQDistribution, self).__init__(shape=parent_q.output_shape)
+        self.sample, self.log_jacobian = transform(parent_q.sample)
+
+        # an "implicit" transformation is a formal object associated with a deterministic
+        # transformation in the model, where the parent q distribution is associated with
+        # the untransformed variable. In this case the ELBO expectations are with respect
+        # to the parent Q distribution, so the jacobian of the transformation does not appear.
+        # By contrast, a non-implicit use of a transformed Q distribution would be to create
+        # a new type of distribution (eg, lognormal by exponentiating a Gaussian parent)
+        # that could then itself be associated with stochastic variables in the graph.
+        self.implicit = implicit
+        self.parent_q = parent_q
+        
+    def sample_stochastic_inputs(self):
+        return {}
+    
+    def entropy(self):
+        if self.implicit:
+            return 0.0
+        else:
+            return self.parent_q.entropy() + self.log_jacobian
+        
+        
 class GaussianQDistribution(QDistribution):
     
-    def __init__(self, shape):
+    def __init__(self, shape, monte_carlo_entropy=False):
         super(GaussianQDistribution, self).__init__(shape=shape)
         
         init_mean = np.float32(np.random.randn(*shape))
@@ -51,8 +95,14 @@ class GaussianQDistribution(QDistribution):
         self.variance = tf.square(self.stddev)        
         self.stochastic_eps = tf.placeholder(dtype=self.mean.dtype, shape=shape, name="eps")
         self.sample = self.stochastic_eps * self.stddev + self.mean        
-        
-        self._entropy = tf.reduce_sum(bf.dists.gaussian_entropy(variance=self.variance))
+
+        # HACK
+        if monte_carlo_entropy:
+            self._entropy = -(tf.reduce_sum(bf.dists.gaussian_log_density(self.sample, mean=self.mean, variance=self.variance)))
+        else:
+            self._entropy = tf.reduce_sum(bf.dists.gaussian_entropy(variance=self.variance))
+
+
         
     def sample_stochastic_inputs(self):
         sampled_eps = np.random.randn(*self.output_shape)
