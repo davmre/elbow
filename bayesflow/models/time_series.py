d14061b7576a92c1b5f9a9af6ecbb963ad914b71
Dave Moore <dmoore@cs.berkeley.edu>
2016-05-28 17:52:34 -0700

    option to omit observation model and create a basic linear-gaussian Markov chain

--- bayesflow/models/time_series.py
+++ bayesflow/models/time_series.py
@@ -10,16 +10,19 @@ import scipy.stats
 
 class LinearGaussian(ConditionalDistribution):
     
-    def __init__(self, prior_mean, prior_cov,
+    def __init__(self, T, prior_mean, prior_cov,
                  transition_mat, transition_mean, transition_cov,
-                 observation_mat, observation_mean, observation_cov, T,
+                 observation_mat=None, observation_mean=None, observation_cov=None,
                  **kwargs):
         # a linear Gaussian state-space model (aka Kalman filter) with dimensions
-        # T: number of time steps
-        # D: dimension of hidden state
-        # K: dimension of output
-        self.T = T
+        #   T: number of time steps
+        #   D: dimension of hidden state
+        #   K: dimension of output
+        # We allow K=0 (implied by observation_mat=None) in which case the model is 
+        # just a Markov chain -- this can be useful together or in conjunction with
+        # non-Gaussian (e.g., neural/VAE) observation models.
         
+        self.T = T
         super(LinearGaussian, self).__init__(prior_mean=prior_mean, prior_cov=prior_cov,
                                              transition_mat=transition_mat,
                                              transition_mean=transition_mean,
@@ -38,45 +41,54 @@ class LinearGaussian(ConditionalDistribution):
         
     def _compute_shape(self, prior_mean_shape, prior_cov_shape,
                        transition_mat_shape, transition_mean_shape, transition_cov_shape,
-                       observation_mat_shape, observation_mean_shape, observation_cov_shape):
+                       observation_mat_shape=None, observation_mean_shape=None, observation_cov_shape=None):
 
         D1, = prior_mean_shape
         D2, D3 = prior_cov_shape
         D4, D5 = transition_mat_shape
         D6, = transition_mean_shape
         D7, D8 = transition_cov_shape
-        K1, D9 = observation_mat_shape
-        K2, = observation_mean_shape
-        K3, K4 = observation_cov_shape
-        
-        assert(D1 == D2 == D3 == D4 == D5 == D6 == D7 == D8 == D9)
-        assert(K1 == K2 == K3 == K4)
+        assert(D1 == D2 == D3 == D4 == D5 == D6 == D7 == D8)
+        if observation_mat_shape is not None:
+            K1, D9 = observation_mat_shape
+            K2, = observation_mean_shape
+            K3, K4 = observation_cov_shape
+            assert(D9 == D1)
+            assert(K1 == K2 == K3 == K4)
+        else:
+            K1 = D1
+
         return (self.T, K1)
     
     def _compute_dtype(self, prior_mean_dtype, prior_cov_dtype,
                        transition_mat_dtype, transition_mean_dtype, transition_cov_dtype,
-                       observation_mat_dtype, observation_mean_dtype, observation_cov_dtype):
+                       observation_mat_dtype=None, observation_mean_dtype=None, observation_cov_dtype=None):
 
-        assert(prior_mean_dtype == prior_cov_dtype == transition_mat_dtype == transition_mean_dtype == transition_cov_dtype == observation_mat_dtype == observation_mean_dtype == observation_cov_dtype)
+        assert(prior_mean_dtype == prior_cov_dtype == transition_mat_dtype == transition_mean_dtype == transition_cov_dtype)
+        if observation_mat_dtype is not None:
+            assert(prior_mean_dtype == observation_mat_dtype == observation_mean_dtype == observation_cov_dtype)
         return prior_mean_dtype
         
     def _sample(self, prior_mean, prior_cov,
                  transition_mat, transition_mean, transition_cov,
-                 observation_mat, observation_mean, observation_cov):
+                 observation_mat=None, observation_mean=None, observation_cov=None):
 
         prior = scipy.stats.multivariate_normal(mean=prior_mean, cov=prior_cov)
         state = prior.rvs(1)
 
         transition_noise = scipy.stats.multivariate_normal(mean=transition_mean, cov=transition_cov)
-        observation_noise = scipy.stats.multivariate_normal(mean=observation_mean, cov=observation_cov)
+        if observation_mat is not None:
+            observation_noise = scipy.stats.multivariate_normal(mean=observation_mean, cov=observation_cov)
 
         output = np.empty(self.output_shape, dtype=self.dtype)
         hidden = np.empty((self.T, self.D), dtype=self.dtype)
         for t in range(self.T):
-            pred_obs = np.dot(observation_mat, state)
-            output[t, :] = pred_obs + observation_noise.rvs(1)
+            if observation_mat is not None:
+                pred_obs = np.dot(observation_mat, state)
+                output[t, :] = pred_obs + observation_noise.rvs(1)
+            else:
+                output[t, :] = state
             hidden[t, :] = state
-            
             state = np.dot(transition_mat, state) + transition_noise.rvs(1)
 
         self._sampled_hidden = hidden
@@ -84,10 +96,12 @@ class LinearGaussian(ConditionalDistribution):
     
     def _logp(self, result, prior_mean, prior_cov,
                  transition_mat, transition_mean, transition_cov,
-                 observation_mat, observation_mean, observation_cov):
+                 observation_mat=None, observation_mean=None, observation_cov=None):
     
         # define the Kalman filtering calculation within the TF graph
-        observation_mean = tf.reshape(observation_mean, (self.K, 1))
+        if observation_mean is not None:
+            observation_mean = tf.reshape(observation_mean, (self.K, 1))
+            
         transition_mean = tf.reshape(transition_mean, (self.D, 1))
         
         pred_mean = tf.reshape(prior_mean, (self.D, 1))
@@ -98,18 +112,28 @@ class LinearGaussian(ConditionalDistribution):
         step_logps = []
         observations = tf.unpack(result)
         for t in range(self.T):
-            # TODO optimize this to not use an explicit matrix inverse
-
-            tmp = tf.matmul(observation_mat, pred_cov)
-            S = tf.matmul(tmp, tf.transpose(observation_mat)) + observation_cov
-            Sinv = tf.matrix_inverse(S)
-            gain = tf.matmul(pred_cov, tf.matmul(tf.transpose(observation_mat), Sinv))
-
             obs_t = tf.reshape(observations[t], (self.K, 1))
-            y = obs_t - tf.matmul(observation_mat, pred_mean) - observation_mean
-            updated_mean = pred_mean + tf.matmul(gain, y)
-            updated_cov = pred_cov - tf.matmul(gain, tmp)
-            #step_logp = bf.dists.multivariate_gaussian_log_density(y, 0, S)
+
+            if observation_mat is not None:
+
+                tmp = tf.matmul(observation_mat, pred_cov)
+                S = tf.matmul(tmp, tf.transpose(observation_mat)) + observation_cov
+                # TODO optimize this to not use an explicit matrix inverse
+                #Sinv = tf.matrix_inverse(S)
+                #gain = tf.matmul(pred_cov, tf.matmul(tf.transpose(observation_mat), Sinv))
+
+                # todo worth implementing cholsolve explicitly?
+                gain = tf.matmul(pred_cov, tf.transpose(tf.matrix_solve(S, observation_mat)))
+                
+                y = obs_t - tf.matmul(observation_mat, pred_mean) - observation_mean
+                updated_mean = pred_mean + tf.matmul(gain, y)
+                updated_cov = pred_cov - tf.matmul(gain, tmp)
+            else:
+                updated_mean = obs_t
+                updated_cov = tf.zeros_like(pred_cov)
+                S = pred_cov
+                y = obs_t - pred_mean
+                
             step_logp = bf.dists.multivariate_gaussian_log_density(y, 0, S)
 
             filtered_means.append(updated_mean)
